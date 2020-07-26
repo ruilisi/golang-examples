3c55a04de831179a1d0ee5e3bf3028acabe5eb7f
hophacker <j@paiyou.co>
2020-07-26 16:04:01 +0800

    full example of generate ssl keys and run tls server

---
+++ networking/tls/README.md
@@ -0,0 +1,4 @@
+#### How to Run
+* Run `genkeys.sh` to generate certificate and key pair of specific domain name and IP, e.x. `./genkeys.sh ruilisi ruilisi.com 127.0.0.1`
+* Run `go run server.go`
+* Run `go run client.go`, now the output should be `world`
---
+++ networking/tls/server.go
@@ -0,0 +1,65 @@
+/* TLSEchoServer
+ */
+package main
+
+import (
+	"bufio"
+	"crypto/rand"
+	"crypto/tls"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+
+	cert, err := tls.LoadX509KeyPair("ruilisi-cert.pem", "ruilisi-key.pem")
+	checkError(err)
+	config := tls.Config{Certificates: []tls.Certificate{cert}}
+
+	now := time.Now()
+	config.Time = func() time.Time { return now }
+	config.Rand = rand.Reader
+
+	service := "0.0.0.0:1200"
+
+	listener, err := tls.Listen("tcp", service, &config)
+	checkError(err)
+	fmt.Println("Listening")
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+		fmt.Println("Accepted")
+		go handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	r := bufio.NewReader(conn)
+	for {
+		msg, err := r.ReadString('\n')
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		println(msg)
+		n, err := conn.Write([]byte("world\n"))
+		if err != nil {
+			log.Println(n, err)
+			return
+		}
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Fatal error ", err.Error())
+		os.Exit(1)
+	}
+}
