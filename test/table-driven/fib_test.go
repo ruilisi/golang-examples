e19fcf2f78697e9c1b30c60ca08a1510094a076b
hophacker <j@paiyou.co>
2020-07-27 09:50:23 +0800

    table driven test

---
+++ test/table-driven/fib_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func TestFib(t *testing.T) {
+	var fibTests = []struct {
+		in       int // input
+		expected int // expected result
+	}{
+		{1, 1},
+		{2, 1},
+		{3, 2},
+		{4, 3},
+		{5, 5},
+		{6, 8},
+		{7, 13},
+	}
+
+	for _, tt := range fibTests {
+		actual := Fib(tt.in)
+		if actual != tt.expected {
+			t.Errorf("Fib(%d) = %d; expected %d", tt.in, actual, tt.expected)
+		}
+	}
+}
